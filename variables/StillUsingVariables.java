9aceacefd7e568b019b8c7c584390b51476712de
Vito-567 <111556762+Vito-567@users.noreply.github.com>
2022-09-16 14:16:33 +0800

    Add files via upload

---
+++ variables/StillUsingVariables.java
@@ -0,0 +1,11 @@
+public class StillUsingVariables {
+    public static void main(String[] args) {
+        String Name;
+        int graduationYear;
+
+        graduationYear = 2025;
+        Name = "Vito Wu";
+
+        System.out.println("My name is " + Name + " and I will graduate in the year " + graduationYear + ".");
+    }
+}
---
+++ variables/YourSchedule.java
@@ -0,0 +1,36 @@
+public class YourSchedule {
+
+    public static void main(String[] args) {
+        String course1, course2, course3, course4, course5, course6, course7, course8, teacher1, teacher2, teacher3,
+                teacher4, teacher5, teacher6, teacher7, teacher8;
+
+        course1 = "English";
+        course2 = "Expository writing";
+        course3 = "Math";
+        course4 = "History";
+        course5 = "Physcis";
+        course6 = "Economy";
+        course7 = "Environmental Sci";
+        course8 = "Java";
+        teacher1 = "Ms. Debra";
+        teacher2 = "Ms. Debra";
+        teacher3 = "Ms. Tina";
+        teacher4 = "Ms. Sounia";
+        teacher5 = "Mr. Toner";
+        teacher6 = "Ms. Eleanor";
+        teacher7 = "Ms. Raj";
+        teacher8 = "Ms. Tina";
+
+        System.out.println(" ============================================================");
+        System.out.println("║ 1 ║           " + teacher5 + "        ║       " + course5 + "            ║");
+        System.out.println("║ 2 ║           " + teacher8 + "         ║       " + course8 + "               ║");
+        System.out.println("║ 3 ║           " + teacher1 + "        ║       " + course1 + "            ║");
+        System.out.println("║ 4 ║           " + teacher6 + "      ║       " + course6 + "            ║");
+        System.out.println("║ 5 ║           " + teacher2 + "        ║       " + course2 + " ║");
+        System.out.println("║ 6 ║           " + teacher3 + "         ║       " + course3 + "               ║");
+        System.out.println("║ 7 ║           " + teacher7 + "          ║       " + course7 + "  ║");
+        System.out.println("║ 8 ║           " + teacher4 + "       ║       " + course4 + "            ║");
+        System.out.println(" ============================================================");
+
+    }
+}
