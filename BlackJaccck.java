887932d21df1afd298d5eef652d0d39da32617d6
Vito-567 <111556762+Vito-567@users.noreply.github.com>
2022-11-01 13:31:51 +0800

    Add files via upload

--- BlackJaccck.java
+++ BlackJaccck.java
@@ -3,29 +3,29 @@ import java.util.Random;
 
 class Main {
   public static void BlackJaccck(String[] args) {
-    Scanner keyboard = new Scanner(System.in);
+    Scanner kbScanner = new Scanner(System.in);
     Random r = new Random();
     int player_card1, player_card2;
     int dealer_card1, dealer_card2;
     int player_total, dealer_total;
     int new_card;
 
-    System.out.println("Welcome to Joshua's blackjack program!");
+    System.out.println("Welcome to Vito's blackjack program!");
 
-    dealer_card1 = 2 + r.nextInt(9);
-    dealer_card2 = 2 + r.nextInt(9);
+    dealer_card1 = 1 + r.nextInt(10);
+    dealer_card2 = 1 + r.nextInt(10);
     dealer_total = dealer_card1 + dealer_card2;
-    player_card1 = 2 + r.nextInt(9);
-    player_card2 = 2 + r.nextInt(9);
+    player_card1 = 1 + r.nextInt(10);
+    player_card2 = 1 + r.nextInt(10);
     player_total = player_card1 + player_card2;
 
-    System.out.println("\nYou get a " + player_card1 + " and a " + player_card2 + ".");
-    System.out.println("Your total: " + player_total + ".\n");
+    System.out.println("You get a " + player_card1 + " and a " + player_card2 + ".");
+    System.out.println("Your total is: " + player_total);
     System.out.println("The dealer: " + dealer_card1 + " showing, and a hidden card.");
     System.out.println("His total is hidden, too.\n");
 
     System.out.print("Would you like to \"hit\" or \"stay\"? ");
-    String choice = keyboard.nextLine();
+    String choice = kbScanner.nextLine();
 
     if (choice.equals("hit")) {
       new_card = 2 + r.nextInt(10);
@@ -42,23 +42,23 @@ class Main {
       }
 
       if (player_total <= 21) {
-        System.out.println("Okay, dealer's turn.");
-        System.out.println("His hidden card was a " + dealer_card2);
-        System.out.println("His total was " + dealer_total);
+        System.out.println("Ok, it is dealer's turn.");
+        System.out.println("Dealer's hidden card was a " + dealer_card2);
+        System.out.println("Dealer's total was " + dealer_total);
 
         do {
           if (dealer_total < 17) {
             choice = "hit";
             System.out.println("Dealer chooses to hit.");
             new_card = 2 + r.nextInt(10);
-            System.out.println("He draws a " + new_card);
+            System.out.println("Dealer draws a " + new_card);
             dealer_total += new_card;
-            System.out.println("His total is " + dealer_total);
+            System.out.println("Dealer's total is " + dealer_total);
           } else {
             choice = "stay";
             System.out.println("Dealer stays.\n");
           }
-        } while (dealer_total <= 21 && (!choice.equals("stay")));
+        } while (dealer_total <= 21 & (!choice.equals("stay")));
       }
     }
   }
