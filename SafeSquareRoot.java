6ff8d95a1eb0bafdd6b876f3b24e5661431724c3
Vito-567 <111556762+Vito-567@users.noreply.github.com>
2022-12-04 22:46:52 +0800

    Add files via upload

---
+++ SafeSquareRoot.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class SafeSquareRoot {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("SQUARE ROOT!");
+
+        System.out.print("Enter a number: ");
+        int num = keyboard.nextInt();
+
+        if (num < 0) {
+            do {
+                System.out.println("You can\'t take the square root of a negative number, silly.");
+                System.out.print("Try again: ");
+                num = keyboard.nextInt();
+            } while (num < 0);
+        }
+        double root = Math.sqrt(num);
+        System.out.println("The square root of " + num + " is " + root);
+
+    }
+}
